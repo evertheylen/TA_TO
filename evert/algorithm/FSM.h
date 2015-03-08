befc8a583b834e26bc104e8cb6d4bb4283409bb6
Evert Heylen <evertheylen@gmail.com>
2015-03-08 22:52:43 +0100

    My version, second commit

---
+++ evert/algorithm/FSM.h
@@ -0,0 +1,69 @@
+#include "tinyxml/tinyxml.h"
+#include <vector>
+#include <map>
+#include <string>
+#include <iostream>
+
+#ifndef _FSM
+#define _FSM
+
+std::string readAttribute(TiXmlElement* elem, const char* tag);
+
+std::string readElement(TiXmlElement* elem);
+
+
+class State {
+public:
+	std::string name;
+
+	State(std::string _name);
+
+	State(TiXmlElement* el);
+};
+
+// erm, not used
+class Transition {
+public:
+	State* from;
+	std::string symbol;
+	State* to;
+
+	Transition(State* _from, std::string _symbol, State* _to);
+
+	bool is_epsilon();
+};
+
+class Automaton {
+public:
+	std::string name;
+
+	std::vector<std::string> alphabet;
+	std::map<std::string, State*> states;  // search on name
+	std::map<State*, std::map<std::string, std::vector<State*>>> transitions;
+	State* start;
+	std::vector<State*> accepting;
+
+	/*
+	Automaton(std::vector<std::string> _alphabet,
+	          std::map<std::string, State> _states,
+	          std::map<State*, std::map<std::string, State*>> _transitions,
+	          State* _start,
+	          std::vector<State*> _accepting):
+		alphabet(_alphabet),
+		states(_states),
+		//transitions(_transitions),  // wtf?
+		start(_start)
+		//accepting(_accepting)   // wtf?
+		{
+		
+		transitions = _transitions;
+		accepting = _accepting;
+	}
+	*/
+
+	Automaton(TiXmlDocument& doc);
+	
+	~Automaton();
+};
+
+#endif
