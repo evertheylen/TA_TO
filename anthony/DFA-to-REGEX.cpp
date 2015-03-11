897f2414a10a2f3f44ab92bd363958c09b29c593
Anthony Hermans <hermans.anthony@hotmail.com>
2015-03-11 17:55:40 +0100

    Mijn eigen mapje woop

---
+++ anthony/DFA-to-REGEX.cpp
@@ -0,0 +1,7 @@
+/*
+//Description: Source code for DFA to REGEX
+//Author: Anthony
+//Date: 11/03/15
+*/
+#include "DFA-to-REGEX.h"
+
---
+++ anthony/DFA-to-REGEX.h
@@ -0,0 +1,11 @@
+/*
+//Description: Header for DFA to REGEX
+//Author: Anthony
+//Date: 11/03/15
+*/
+#ifndef ____DFA_to_REGEX__
+#define ____DFA_to_REGEX__
+
+#include <stdio.h>
+
+#endif /* defined(____DFA_to_REGEX__) */
