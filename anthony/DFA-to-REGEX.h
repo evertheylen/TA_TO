2ad3d54c3f199995792a6b485f8ba12901b92ff4
Anthony Hermans <hermans.anthony@hotmail.com>
2015-03-26 20:16:16 +0100

    DFA to regex finished

--- anthony/DFA-to-REGEX.h
+++ anthony/DFA-to-REGEX.h
@@ -1,8 +1,8 @@
 /*
-//Description: Header for DFA to REGEX
-//Author: Anthony Hermans
-//Date: 11/03/15
-*/
+ *Description: Header for DFA to REGEX
+ *Author: Anthony Hermans
+ *Date: 26/03/15
+ */
 #ifndef ____DFA_to_REGEX__
 #define ____DFA_to_REGEX__
 
@@ -11,43 +11,54 @@
 #include <vector>
 #include "TinyXML/tinyxml.h"
 
+//CLASS STATE
 class State{
 private:
-std::string name;
+std::string name;	//Name of the state
 public:
+//Getters
 void setName(std::string new_name);
 std::string getName();
+//Constructors
 State(std::string name);
 State();
 };
 
-
+//CLASS TRANSITION
 class Transition{
 private:
-State* _begin;
-State* _end;
-std::string _label;
+State* _begin;		//Beginstate of the transition
+State* _end;		//Endstate of the transition
+std::string _label;	//Label of the transition
 public:
+//Getters and setters
 std::string getLabel();
-void setLabel(std::string newlabel);
 State* get_from();
 State* get_to();
+void setLabel(std::string newlabel);
+//Constructor and destructor
 Transition(State* begin, std::string label, State* end);
 ~Transition();
 };
 
+//CLASS AUTOMATON
 class Automaton{
 private:
-std::string name;
+std::string name;						//Name of the automaton
 public:
+//Constructor
 Automaton();
-std::vector<State*> start;
-std::vector<State*> end;
-std::vector<State*> total;
-std::vector<Transition*> transitions;
+//Public attributes
+std::vector<State*> start;				//Startstates of the automaton
+std::vector<State*> end;				//Endstates of the automaton
+std::vector<State*> total;				//Total of states of the automaton
+std::vector<Transition*> transitions;	//Total of transition of the automaton
+//Getters and setters
 void setName(std::string new_name);
 std::string getName();
+//Read from XML and put information in automaton
 int readXML(TiXmlDocument input_automaton);
+//The conversion of the DFA to the regex expression
 std::string StateEliminationTechnique();
 };
 
--- anthony/main.cpp
+++ anthony/main.cpp
@@ -1,8 +1,9 @@
 /*
-//Description: Source code for main
-//Author: Anthony Hermans
-//Date: 11/03/15
-*/
+ *Description: Main for DFA to REGEX
+ *Author: Anthony Hermans
+ *Date: 26/03/15
+ */
+ 
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
@@ -12,22 +13,23 @@ using namespace std;
 
 int main(int argc, char const* argv[]){
 	cout << "Converting a DFA to REGEX using the state elimination technique.\n";
-	TiXmlDocument inputDFA;
+	TiXmlDocument input_DFA;
 	if (argc == 1){
 		cerr << "U need to specify an input DFA.\n";
 		return 0;
 	}
-	if (inputDFA.LoadFile(argv[1]) == false){
+	if (input_DFA.LoadFile(argv[1]) == false){
 		cerr << "Problems with the loading of the DFA ==> shut down.\n";
 		return 0;
 	}
 	
 	Automaton DFA;
-	DFA.readXML(inputDFA);
+	DFA.readXML(input_DFA);
 
+	cout << "The regex expression of the DFA can be found in the file: FinalRegex.txt\n";
 	ofstream regex_out;
-	regex_out.open("REGEX.txt");
-	regex_out << "This text file contains the regex expression of the specified inputDFA:\n";
+	regex_out.open("FinalRegex.txt");
+	regex_out << "This text file contains the regex expression of the specified DFA:\n";
 	regex_out << DFA.StateEliminationTechnique();
 	regex_out.close();
 	
