07a23db45c02f6fda9e220cb8f41fb064957b623
Anthony Hermans <hermans.anthony@hotmail.com>
2015-04-06 15:15:37 +0200

    vage makefile error

--- fase1/RE-eNFA/RE-eNFA.cpp
+++ fase1/RE-eNFA/RE-eNFA.cpp
@@ -1,45 +1,76 @@
 #include "RE-eNFA.h"
-#include "../lib/FSM/FSM.h"
+#include "FSM.h"
 
 #include <string>
 #include <stack>
 #include <iostream>
+#include <fstream>
+#include <set>
 using namespace std;
 
 s_eNFA RE_to_eNFA(std::string& str) {
-	s_eNFA N({"q0"},//states
-			{'e'},		//alphabet
-			0, {0});//from -> to
-	stack <char> brackets;
+	s_eNFA N({"q0"},				//staten
+			{'e'},					//alfabet
+			0, {0});				//from -> to
+	stack <int> brackets;			//Helpt bij het behouden startstaat, dus juiste beginpunt
 	int states=1;
-	for (string::iterator it = str.begin(); it!= str.end(); ++it){
+	int current_start_state = N.ID("q0"); //Houdt de ID van de staat bij waar je moet beginnen
+	int current_end_state;
+	cout << "Converting the regex string" << endl;
+	for (string::iterator it = str.begin(); it!= str.end(); it++){
 		cout << *it << endl;
 		if (*it == '('){
-			brackets.push(*it);
+			brackets.push(current_start_state);
 			continue;
 		}
 		if (*it == ')'){
+			current_start_state = brackets.top();
 			brackets.pop();
 			continue;
 		}
 		if (*it == '+'){
+			//Hier moet de juiste staat terug gezet worden
+			
 			continue;
 		}
 		if (*it == '*'){
+			//Random e pijlen toevoegen
 			continue;
 		}
 		else{
-			N.sigma.insert(*it);
-			string new_state = "q" + states;
+			//Gaat na of het element in het alfabet zit, zo niet voeg toe
+			if (N.isInSigma(*it) == false){
+				cout << "Inserting in alphabet: " <<  *it << endl;
+				N.sigma.insert(*it);
+			}
+			string new_state1 = "q" + to_string(states);
+			N.add_state(new_state1 ,false);
 			states++;
-			int temp_state;
-			temp_state = N.add_state(new_state, false);
-			N.set_delta(states-1,'e',N.delta(states-1,'e'));
-		
+			string new_state2 = "q" + to_string(states);
+			N.add_state(new_state2 ,false);
+			states++;
+			string new_state3 = "q" + to_string(states);
+			N.add_state(new_state3 ,false);
+			states++;
+			set<int> temp = N.delta(current_start_state,'e');
+			temp.insert(N.ID(new_state1));
+			N.set_delta(current_start_state, 'e', temp);
+			temp = N.delta(N.ID(new_state1),*it);
+			temp.insert(N.ID(new_state2));
+			N.set_delta(N.ID(new_state1), *it , temp);
+			temp = N.delta(N.ID(new_state2),'e');
+			temp.insert(current_end_state);
+			N.set_delta(N.ID(new_state2), 'e' , temp);
 		}
 	}
+	cout << "Het aantal staten = " << states << endl;
 	
+	//Tijdelijk printen naar dot
+	std::ofstream test;
+	test.open("RE_eNFA.dot");
+	N.to_dot(test);
+	test.close();
 	
-	
+	//Geeft overeenkomende eNFA terug
 	return N;
 }
--- fase1/RE-eNFA/RE-eNFA.h
+++ fase1/RE-eNFA/RE-eNFA.h
@@ -1,4 +1,4 @@
-#include "../lib/FSM/FSM.h"
+#include "FSM.h"
 #include <string>
 
 s_eNFA RE_to_eNFA(std::string& str);
