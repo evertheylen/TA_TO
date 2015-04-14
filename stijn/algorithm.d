d26321280310ebdabda732c99cfa2682e2c5fd99
Stijn Janssens <janssens.stijn@hotmail.com>
2015-04-14 22:36:40 +0200

    update op verlof

--- stijn/algorithm.d
+++
@@ -1,2 +0,0 @@
-algorithm.o: algorithm.cpp lib/tinyxml.h lib/tinystr.h algorithm.h
-	$(CC) $(CXXFLAGS) -c $< -o $@
--- stijn/lib/tinystr.d
+++
@@ -1,2 +0,0 @@
-tinystr.o: lib/tinystr.cpp lib/tinystr.h
-	$(CC) $(CXXFLAGS) -c $< -o $@
