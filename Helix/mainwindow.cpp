5defe58cf3a141af4a80bdfa7a53ba148766b291
Anthony Hermans <hermans.anthony@hotmail.com>
2015-05-12 14:34:41 +0200

    gui starts

--- Helix/mainwindow.cpp
+++ Helix/mainwindow.cpp
@@ -1,14 +1,32 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "secdialog.h"
+#include "QtGui"
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    secDialog secdialog;
+    secdialog.setWindowTitle("The real Helix");
+    secdialog.setModal(true);
+    secdialog.exec();
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    this->close();
 }
---
+++ Helix/secdialog.cpp
@@ -0,0 +1,19 @@
+#include "secdialog.h"
+#include "ui_secdialog.h"
+
+secDialog::secDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::secDialog)
+{
+    ui->setupUi(this);
+}
+
+secDialog::~secDialog()
+{
+    delete ui;
+}
+
+void secDialog::on_pushButton_clicked()
+{
+    this->close();
+}
