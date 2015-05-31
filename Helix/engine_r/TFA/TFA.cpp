f0ecf744b9632a9455f6cbb8ba3cae695c2d1589
RoyalOpponent <Pieter@Pieters-MacBook-Pro.local>
2015-05-31 16:13:20 +0200

    small changes

--- Helix/engine_r/TFA/TFA.cpp
+++ Helix/engine_r/TFA/TFA.cpp
@@ -44,7 +44,7 @@ Table::Table(int num):
 void Table::print(ostream& out, s_DFA& D) {
 	out << "\t";
 	for (int ss=1; ss<num_states; ss++) {
-		out << D.realState(ss) << "\t";
+        out << D.realState(ss) << "\t";
 	}
 	out << "\n";
 	
@@ -69,111 +69,8 @@ bool operator!=(Table& lhs, Table& rhs) {
 	return lhs.data != rhs.data;
 }
 
-
-
-bool block::operator==(block const& b) {
-	if (b.state1 == this->state1 and b.state2 == this->state2) {
-		return true;
-	} else return false;
-}
-
-// name is weird :p
-bool block::hasFinal(s_DFA D) {
-	if (D.isFinal(state1) == true xor D.isFinal(state2) == true) return true;
-	else return false;
-}
-// ^ equivalent to D.isFinal(state1) != D.isFinal(state2)
-
-
-//this erases the same blocks in a vector
-void filter(vector<block>& table_vec) {
-	vector<block> copy_vec = table_vec;
-	for (int i =0; i < table_vec.size(); i++) {
-		for (int e=0; e < copy_vec.size(); e++) {
-			if (table_vec.at(i) == copy_vec.at(e)) {
-				table_vec.erase(table_vec.begin() + i);
-			}
-		}
-	}
-	return;
-}
-
-vector<block> makeTable(s_DFA D) {
-	vector<block> table_vec;
-	for (int e=0; e <= D.num_states+1; e++) {
-		for (int i=0; i<= D.num_states+1; i++) {
-			if (i == e) continue;
-			block tempblock(e,i);
-			if (tempblock.hasFinal(D) == true) tempblock.dist = true;
-			table_vec.push_back(tempblock);
-		}
-		filter(table_vec);
-	}
-	return table_vec;
-}
-
-
-void makePairs(s_DFA D, vector<block> table_vec) {
-	for (vector<block>::iterator it = table_vec.begin(); it != table_vec.end(); ++it) {
-		for (char a: D.sigma) {
-			int nextState1 = D.delta(it->state1,a);
-			int nextState2 = D.delta(it->state2,a);
-			if (D.isFinal(nextState1) != D.isFinal(nextState2)) {
-				it->dist = true;
-				break;
-			}
-		}
-	}
-}
-
-vector<block> removeDist(vector<block> table_vec) {
-	vector<block> new_vector;
-	for (vector<block>::iterator it = table_vec.begin(); it != table_vec.end(); ++it) {
-		if (it->dist == false) {
-			new_vector.push_back(*it);
-		}
-	}
-	return new_vector;
-}
-
-s_DFA makeNewDFA(s_DFA D, vector<block> table) {
-	s_DFA mini;
-	for (int i=0; i < table.size(); i++) {
-		block tempBlock = table.at(i);
-		string new_name = "{" + D.realState(tempBlock.state1) + ", " + D.realState(tempBlock.state2) + "}";
-		mini.add_state(new_name, false);
-		for (char a: D.sigma) {
-			int old1 = D.delta(tempBlock.state1, a);
-			int old2 = D.delta(tempBlock.state2, a);
-			string tempName = "{" + D.realState(old1) + ", " + D.realState(old2) + "}";
-			mini.set_delta(D.ID(new_name),a,D.ID(tempName)); // waar gaat mijn nieuwe delta naartoe? naar de combinatie van old1 en old2 OPGELOST!
-		}
-	}
-	return mini;
-}
-
 s_DFA TFA(s_DFA D) {
-	/*
-	int n = D.num_states+1;
-	// Step 0 --> should work
 	D.lazy_evaluation();
-	
-	// Step 1: create the table and mark all the pairs with a final and a nonfinal state
-	// rather simple, should work
-	vector<block> table = makeTable(D);
-	
-	// Step 2: mark all the rest and build the equivalent 'block'
-	// ...
-	makePairs(D,table);
-	
-	table = removeDist(table);
-	// Step 3: reconstruct DFA based on the new 'blocks'
-	s_DFA optimusDFA = makeNewDFA(D, table);
-	return optimusDFA;
-	*/
-	
-	D.lazy_evaluation();
-	
 	// Create the table
 	Table table(D.num_states);
 	table.print(std::cout, D);
@@ -253,6 +150,14 @@ s_DFA TFA(s_DFA D) {
 		tempnames.at(grouped_s) += D.realState(s);
 		tempnames.at(grouped_s) += ", ";
 	}
+    
+    for (int s=0; s<D.num_states; s++) {
+        int grouped_s = groups[s];
+        if(tempnames.at(grouped_s).back() == ' '){
+            tempnames.at(grouped_s).pop_back();
+            tempnames.at(grouped_s).pop_back();
+        }
+    }
 	
 	optimus_prime.q0 = groups[D.q0];
 	
