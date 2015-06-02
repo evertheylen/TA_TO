eb0fbfb6dcf966ec476b6f5225897aac9e6c515b
Stijn Janssens <janssens.stijn@hotmail.com>
2015-06-02 15:38:20 +0200

    PRoduct????

--- Helix/tests/test_S_engine.cpp
+++ Helix/tests/test_S_engine.cpp
@@ -2,13 +2,16 @@
 #include "gtest/gtest.h"
 #include "tinyxml.h"
 #include "FSM.h"
+#include "../etc/file.h"
 #include "filecompare.h"
+#include <iostream>
+#include <fstream>
 
 // Test suffixtrees here
-
-File f(../"miss2.txt");
+/*
+File f("../miss2.txt");
 std::ofstream file;
 file.open("miss2.dot");
 file << *f.suffixtree;
 file.close();
-EXPECT_TRUE(fileCompare("miss2.dot","miss2_referentie.dot"));
+EXPECT_TRUE(fileCompare("miss2.dot","miss2_referentie.dot"));*/
